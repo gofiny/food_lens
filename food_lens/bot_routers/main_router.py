04f9671111dd509115f959217d3b785fb3c760f0
gofiny <gofiny@inbox.ru>
2024-04-04 11:57:13 +0700

    test callbacks

--- food_lens/bot_routers/main_router.py
+++ food_lens/bot_routers/main_router.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 from aiogram.methods import TelegramMethod
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram.types.keyboard_button import KeyboardButton
 from aiogram.types.inline_keyboard_button import InlineKeyboardButton
@@ -15,20 +15,45 @@ class MFilter(Filter):
     async def __call__(self, message: Message) -> bool:
         return True
 
-@main_router.message(MFilter())
-async def handle_button_one(message: Message):
-    await message.edit_text(text="new_text")
+
+class CFilter(Filter):
+    def __init__(self, data: str):
+        self._data = data
+
+    async def __call__(self, callback: CallbackQuery) -> bool:
+        return callback.data == self._data
 
 
-@main_router.message(F.text == "button two")
-async def handle_button_one(message: Message):
-    pass
+@main_router.callback_query(CFilter("one"))
+async def handle_button_one(callback: CallbackQuery):
+    inline_keyboard = InlineKeyboardBuilder()
+    inline_keyboard.button(text="new button", callback_data="new_button")
+
+    await callback.message.edit_text(text="new_text", reply_markup=inline_keyboard.as_markup())
+
+
+@main_router.callback_query(CFilter("two"))
+async def handle_button_one(callback: CallbackQuery):
+    inline_keyboard = InlineKeyboardBuilder()
+    inline_keyboard.button(text="new two button", callback_data="new_button_2")
+
+    await callback.message.edit_text(text="new_text", reply_markup=inline_keyboard.as_markup())
+
+
+@main_router.callback_query(CFilter("new_button"))
+@main_router.callback_query(CFilter("new_button_2"))
+async def handle_button_one(callback: CallbackQuery):
+    inline_keyboard = InlineKeyboardBuilder()
+    inline_keyboard.button(text="button one", callback_data="one")
+    inline_keyboard.button(text="button two", callback_data="two")
+
+    await callback.message.edit_text(text="new_text", reply_markup=inline_keyboard.as_markup())
 
 
 @main_router.message()
 async def no_answer(message: Message) -> TelegramMethod[Message]:
     inline_keyboard = InlineKeyboardBuilder()
-    inline_keyboard.button(text="button one", callback_data="test")
-    inline_keyboard.button(text="button two", callback_data="test")
+    inline_keyboard.button(text="button one", callback_data="one")
+    inline_keyboard.button(text="button two", callback_data="two")
 
     return message.answer("Some text", reply_markup=inline_keyboard.as_markup())
